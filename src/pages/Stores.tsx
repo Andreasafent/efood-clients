18488d8e9235da5e3cc1beb79402e217c79f12cb
Andreasafent <afentoulidisandreas@gmail.com>
2025-03-21 14:35:33 +0200

    Added stores axios response

--- src/pages/Stores.tsx
+++ src/pages/Stores.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import axiosInstance from "../api/axiosInstance";
+import { CategoriesResponse, Category } from "../types/categories";
+
 function Stores(){
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    useEffect(()=>{
+        axiosInstance.get<CategoriesResponse>("/client/categories")
+            .then((response)=>{
+                if(!response.data.success){
+                    return;
+                }
+                setCategories(response.data.data.categories);
+            })
+    })
     return 'Stores';
 }
 
